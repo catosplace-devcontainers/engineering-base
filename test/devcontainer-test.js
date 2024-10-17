25d1232e1680503f5d8634e4f1ae0bf548b8dfd3
Peter Sellars <psellars@gmail.com>
2024-10-17 14:29:34 +0000

    feat(testcontainers): initial testcontainers work
    * Added ability to install Docker into devcontainer via the build args
    * Added the ability to mount the /var/run/docker.sock into the devcontainer
    * Added the @devcontainers/cli to the node packages
    * Added the testcontainers and mocha packages to node
    * Implemented an initial test that builds the devcontainer at this time

---
+++ test/devcontainer-test.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process');
+
+describe('DevContainer Tests', function () {
+  this.timeout(10000);
+
+  it('should build the DevContainer', function (done) {
+    try {
+      const output = execSync('npx @devcontainers/cli build --workspace-folder . .');
+      console.log(output.toString());
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
